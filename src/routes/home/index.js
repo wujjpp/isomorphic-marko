d674ab73fb57b506192139ebd09490c0cc20040d
Wu Jian Ping <wujjpp@hotmail.com>
2017-03-23 16:17:05 +0800

    add comments in /src/routes/home/index.js

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,7 +4,7 @@ import assets from '../../assets-loader'
 export default function(req, res) {
 
   let context = req.context.assign({
-    assets: assets.home,
+    assets: assets.home, //NOTE: please take note on entry-settings.js, the "home" arrtibute is from there
     initCount: 10
   });
 
