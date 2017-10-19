ec9538dd231b25b676951aff085cf5b3f89c7745
Wu Jian Ping <wujjpp@hotmail.com>
2017-10-19 10:09:12 +0800

    .

--- src/routes/issue-getEl/components/app/component.js
+++ src/routes/issue-getEl/components/app/component.js
@@ -15,20 +15,20 @@ export default class Component {
 
     console.log('---------------------------------------------------------') // eslint-disable-line
 
-    let otherId = this.getElId('other')
-    let other = this.getEl('other')
+    let other = this.getComponent('other')
+    let otherId = other.id
     // you can obtain the id of the "other" component, but you cannot get it's DOM by key
     console.log('other\'s id ---> ', otherId) // eslint-disable-line
-    console.log('DOM other ---> ', other) // eslint-disable-line
+    console.log('DOM other ---> ', other.el) // eslint-disable-line
     console.log('you can obtain the id of the "other" component, but you cannot get it\'s DOM element by key') // eslint-disable-line
 
     console.log('---------------------------------------------------------') // eslint-disable-line
 
-    let containerId = this.getEl('container')
-    let container = this.getEl('container')
+    let container = this.getComponent('container')
+    let containerId = container.id
     // you can not obtain id and DOM element of "container" component
     console.log('container\'s id ---> ', containerId) // eslint-disable-line
-    console.log('DOM container --->', container) // eslint-disable-line
+    console.log('DOM container --->', container.el) // eslint-disable-line
     console.log('you can not obtain id and DOM element of "container" component') // eslint-disable-line
   }
 }
