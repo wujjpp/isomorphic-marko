27ed800e1f9b348794e743c0ee4a5bc614f9ce85
Wu Jian Ping <wujjpp@hotmail.com>
2017-10-19 10:14:43 +0800

    .

--- src/routes/issue-getEl/components/app-container/component.js
+++ src/routes/issue-getEl/components/app-container/component.js
@@ -1,5 +1,11 @@
 export default class Component {
   onMount() {
     console.log('app-container onMount called') // eslint-disable-line
+
+    console.log(this.getEl('btn-in-app-container')) // eslint-disable-line
+  }
+
+  btnClick() {
+    console.log('btn-in-app-container clicked') // eslint-disable-line
   }
 }
