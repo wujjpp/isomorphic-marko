3a14bf5d8a419f7d8fae781538eb54b16c51f532
Wu Jian Ping <wujjpp@hotmail.com>
2017-10-20 13:31:11 +0800

    issue-component-key

--- src/routes/issue-component-key/components/app/component.js
+++ src/routes/issue-component-key/components/app/component.js
@@ -7,8 +7,10 @@ import appDialog from '../app-dialog'
 
 export default class TestComponent {
   onMount() {
-    console.log(this.getEl('btn1'))
-    console.log(this.getElId('btn1'))
+    console.log(this.getEl('btn1')) // eslint-disable-line
+    console.log(this.getElId('btn1')) // eslint-disable-line
+    console.log(this.getElId('btn999-rendered-id')) // eslint-disable-line
+    console.log(this.getEl('name')) // eslint-disable-line
   }
 
   openDialog() {
